cbe23d6a06164499c616c55f0a58cdcae6d74b4f
Patrik Suomi <patrik.suomi@edu.turkuamk.fi>
2025-12-17 22:31:08 +0200

    add: added Saalisraportti to Kalastaja constructor

--- Kalastaja.java
+++ Kalastaja.java
@@ -7,19 +7,25 @@ public class Kalastaja {
     private Viehe viehe;
 
     /*
+    Muuttuja kalastajan saalisraportille.
+    */
+    private Saalisraportti raportti;
+
+    /*
     Konstruktori kalastajalle.
 
-    Asettaa kalastajalle perusvieheen.
+    Asettaa kalastajalle perusvieheen sekä saalisraportin.
 
     AE: true
     */
     public Kalastaja() {
         this.viehe = new Viehe();
+        raportti = new Saalisraportti();
     } 
 
     /*
     Ottaa vastaan parametrinä vieheen, joka asetetaan
-    kalastajan vieheeksi.
+    kalastajan nykyiseksi.
 
     AE: true
     */
