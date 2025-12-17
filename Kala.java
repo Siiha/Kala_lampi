37d203c507837ebe52f449fe98cac3b1b137f51a
Siina <siina.hakala@edu.turkuamk.fi>
2025-12-17 20:55:27 +0200

    fix: add constructor to Kala class and initialize 'koko' field

--- Kala.java
+++ Kala.java
@@ -1,6 +1,9 @@
 package Kala_lampi;
 public class Kala  extends Elio {
-    public Kala() {
+    private double koko;
+    public Kala(double koko) {
+        super(koko);
+        this.koko = koko;
     }
     public void syo(Elio saalis) {
         if (saalis.annettuKoko() < this.annettuKoko()) {
