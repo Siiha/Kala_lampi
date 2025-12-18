78002cecc17475fe94c5d6169d205ea43100fefd
Siina <siina.hakala@edu.turkuamk.fi>
2025-12-18 16:37:37 +0200

    fix: update Kala and Kasvi classes to include prey list and modify growth method

--- Kasvi.java
+++ Kasvi.java
@@ -1,13 +1,22 @@
+
 package Kala_lampi;
+import Java.util.ArrayList,Java.util.Random;
 public class Kasvi {
     private double koko;
     private double kasvuNopeus;
-    private boolean kuollut=false; 
+    private boolean kuollut=false;
     public Kasvi(double koko) {
         this.koko = koko;
     }
-    public void kasva() {
-        this.koko += kasvuNopeus;
+    public void kasva(ArrayList<Kala> Kuolleet_kalat) {
+        int Kks = Kuolleet_kalat.size();
+        if (Kks > 0){
+            Random rd = new Random();
+            int id = rd.nextInt(Kks); 
+            Kala kala = Kuolleet_kalat.get(id);
+            this.koko += kala.annettuKoko() / 10;
+            Kuolleet_kalat.remove(id);
+        }
     }
     public double annettuKoko() {
         return this.koko;
