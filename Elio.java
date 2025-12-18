12d3b52a17ec87812e15d716ad7b57f434770313
Patrik Suomi <patrik.suomi@edu.turkuamk.fi>
2025-12-18 21:30:42 +0200

    add: created arvoElionSukupuoli, arvoElionKoko and luoElio methods for Elio

--- Elio.java
+++ Elio.java
@@ -1,12 +1,39 @@
 package Kala_lampi;
+
+import java.util.ArrayList;
+
 public class Elio {
     public boolean kuollut=false;
     protected double koko;
     protected boolean sukupuoli; // true = naaras, false = uros
-    public Elio(double koko, boolean sukupuoli) {
+    protected ArrayList<Kuolevainen> saaliit;
+    private static double minAlkuKoko = 5.0;
+    private static double maxAlkuKoko = 7.0;
+
+    public Elio(double koko, boolean sukupuoli, ArrayList<Kuolevainen> saaliit) {
         this.koko = koko;
         this.sukupuoli = sukupuoli;
+        this.saaliit = saaliit;
     }
+
+    private static boolean arvoElionSukupuoli() {
+        int rng = (int)Math.round(Math.random());
+        if (rng == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    private static double arvoElionKoko() {
+        double koko = minAlkuKoko + (Math.random() * (maxAlkuKoko - minAlkuKoko));
+        return Math.round(koko * 10.0) / 10.0;
+    }
+
+    public static Elio luoElio(ArrayList<Kuolevainen> saalislista) {
+        Elio eräs = new Elio(arvoElionKoko(), arvoElionSukupuoli(), saalislista);
+        return eräs;
+    }
+
     public double annettuKoko() {
         return this.koko;
     }
