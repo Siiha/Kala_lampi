773fcb1c7a446c417f5ea20c29fe41cb9bdf1af5
Patrik Suomi <patrik.suomi@edu.turkuamk.fi>
2025-12-18 22:12:00 +0200

    fix: some type fixes and implemented Kuolevainen interface to Elio

--- Elio.java
+++ Elio.java
@@ -2,21 +2,24 @@ package Kala_lampi;
 
 import java.util.ArrayList;
 
-public class Elio {
+public class Elio implements Kuolevainen {
     public boolean kuollut=false;
     protected double koko;
     protected boolean sukupuoli; // true = naaras, false = uros
-    protected ArrayList<Kuolevainen> saaliit;
+    private ArrayList<Kasvi> saaliit;
     private static double minAlkuKoko = 5.0;
     private static double maxAlkuKoko = 7.0;
 
-    public Elio(double koko, boolean sukupuoli, ArrayList<Kuolevainen> saaliit) {
+    public Elio(double koko, boolean sukupuoli) {
         this.koko = koko;
         this.sukupuoli = sukupuoli;
+    }
+
+    public void asetaSaaliit(ArrayList<Kasvi> saaliit) {
         this.saaliit = saaliit;
     }
 
-    private static boolean arvoElionSukupuoli() {
+    protected static boolean arvoElionSukupuoli() {
         int rng = (int)Math.round(Math.random());
         if (rng == 0) {
             return false;
@@ -29,8 +32,8 @@ public class Elio {
         return Math.round(koko * 10.0) / 10.0;
     }
 
-    public static Elio luoElio(ArrayList<Kuolevainen> saalislista) {
-        Elio eräs = new Elio(arvoElionKoko(), arvoElionSukupuoli(), saalislista);
+    public static Elio luoElio(ArrayList<Kasvi> saalislista) {
+        Elio eräs = new Elio(arvoElionKoko(), arvoElionSukupuoli());
         return eräs;
     }
 
