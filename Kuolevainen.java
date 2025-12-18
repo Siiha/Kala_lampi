6e30a5ea187779a592fea504543317f3b6db759e
Patrik Suomi <patrik.suomi@edu.turkuamk.fi>
2025-12-18 13:26:54 +0200

    add: creation of Kuolevainen interface with methods onkoKuollut and olionAktiviteetti

---
+++ Kuolevainen.java
@@ -0,0 +1,14 @@
+package Kala_lampi;
+
+public interface Kuolevainen {
+    
+    /*
+    Objekti ilmoittaa kuolleisuuden tilastaan.
+    */
+    public boolean onkoKuollut();
+
+    /*
+    Objektin aktiviteetin toteutus aikayksikön sisällä.
+    */
+    public void olionAktiviteetti();
+}
