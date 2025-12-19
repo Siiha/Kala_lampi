46a027831ccff4adeee041da4a88b7891e3548b3
Patrik Suomi <patrik.suomi@edu.turkuamk.fi>
2025-12-19 16:56:05 +0200

    fix: removed the parameters for Elio luoElio method, removed Petokala saalistusteho and added creation loops for Vesistö constructor

--- Petokala.java
+++ Petokala.java
@@ -1,15 +1,15 @@
 package Kala_lampi;
 import java.util.ArrayList;
-import java.util.Random;
+
 public class Petokala extends Kala {
-    private double saalistusTeho;
+
     private static double minAlkuKoko = 12.0;
     private static double maxAlkuKoko = 15.0;
 
-    public Petokala(double koko, boolean sukupuoli, ArrayList<Elio> saaliit,ArrayList<Kala> Kala_saaliit, double saalistusTeho) {
+    public Petokala(double koko, boolean sukupuoli, ArrayList<Elio> saaliit,ArrayList<Kala> Kala_saaliit) {
         super(koko,sukupuoli,saaliit);
         this.saaliit.addAll(Kala_saaliit);
-        this.saalistusTeho = saalistusTeho;
+
     }
 
     private static double arvoElionKoko() {
@@ -17,8 +17,8 @@ public class Petokala extends Kala {
         return Math.round(koko * 10.0) / 10.0;
     }
 
-    public static Petokala luoPetokala(ArrayList<Elio> saaliit, ArrayList<Kala> Kala_saaliit, double saalistusTeho) {
-        Petokala eräs = new Petokala(arvoElionKoko(), arvoElionSukupuoli(), saaliit, Kala_saaliit, saalistusTeho);
+    public static Petokala luoPetokala(ArrayList<Elio> saaliit, ArrayList<Kala> Kala_saaliit) {
+        Petokala eräs = new Petokala(arvoElionKoko(), arvoElionSukupuoli(), saaliit, Kala_saaliit);
         return eräs;
     }
 }
