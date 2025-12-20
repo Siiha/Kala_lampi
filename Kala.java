d84a0d6c187bd4425f8da2bc4b5aa07d2fcdb430
Patrik Suomi <patrik.suomi@edu.turkuamk.fi>
2025-12-20 17:49:45 +0200

    add: write the variable and method definitions

--- Kala.java
+++ Kala.java
@@ -2,24 +2,58 @@ package Kala_lampi;
 import java.util.ArrayList;
 import java.util.Random;
 public class Kala  extends Elio {
-    protected ArrayList<Elio> saaliit; // lista mahdollisista eliöistä joita kala voi syödä
+    /*
+    Muuttuja kalan mahdollisille saaliille.
+    */
+    protected ArrayList<Elio> saaliit;
+
+    /*
+    Muuttuja kalan minimikoolle.
+    */
     private static double minAlkuKoko = 8.0;
+
+    /*
+    Muuttuja kalan maksimikoolle.
+    */
     private static double maxAlkuKoko = 10.0;
+
+    /*
+    Kalan konstruktori.
+    Ottaa vastaan parametreinä kalan koon, sukupuolen
+    ja referenssin saalislistalle.
+
+    AE: true
+    */
     protected Kala(double koko, boolean sukupuoli, ArrayList<Elio> saaliit) {
         super(koko, sukupuoli);
         this.saaliit = saaliit;
     }
 
+    /*
+    Arpoo kalalle aloituskoon.
+
+    AE: true
+    */
     private static double arvoElionKoko() {
         double koko = minAlkuKoko + (Math.random() * (maxAlkuKoko - minAlkuKoko));
         return Math.round(koko * 10.0) / 10.0;
     }
 
+    /*
+    Luo uuden kalan ja palauttaa sen.
+
+    AE: true
+    */
     public static Kala luoKala(ArrayList<Elio> saalislista) {
         Kala eräs = new Kala(arvoElionKoko(), arvoElionSukupuoli(), saalislista);
         return eräs;
     }
 
+    /*
+    Kala yrittää löytää ja syödä saaliin.
+
+    AE: saaliit.size() > 0
+    */
     public void syo() {
         int ss = saaliit.size();
         Random rd = new Random();
@@ -33,10 +67,6 @@ public class Kala  extends Elio {
             this.koko += saalis.annettuKoko() / 100;
             saaliit.remove(saalis);
         }
-    }
-
-    public void olionAktiviteetti() {
-        syo();
     }
 }
     
