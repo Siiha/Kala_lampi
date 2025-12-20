e56e0477ffdd64b338f786f92966b4a0df82078b
Patrik Suomi <patrik.suomi@edu.turkuamk.fi>
2025-12-20 17:52:37 +0200

    add: wrote the variable and method definitions

--- Petokala.java
+++ Petokala.java
@@ -2,24 +2,63 @@ package Kala_lampi;
 import java.util.ArrayList;
 import java.util.Random;
 public class Petokala extends Kala {
+    /*
+    Muuttuja satunnaislukugeneraattorille.
+    */
     private static Random rd = new Random();
+
+    /*
+    Muuttuja petokalan minimikoolle.
+    */
     private static double minAlkuKoko = 12.0;
+
+    /*
+    Muuttuja petokalan maksimikoolle.
+    */
     private static double maxAlkuKoko = 15.0;
+
+    /*
+    Muuttuja petokalan mahdollisille saaliille.
+    */
     protected ArrayList<Kala> Kala_saaliit;
+
+    /*
+    Petokalan konstruktori.
+    Ottaa vastaan parametreinä petokalan koon, sukupuolen
+    ja referenssin saalislistalle.
+
+    AE: true
+    */
     protected Petokala(double koko, boolean sukupuoli, ArrayList<Elio> saaliit,ArrayList<Kala> Kala_saaliit) {
         super(koko,sukupuoli,saaliit);
         this.Kala_saaliit = Kala_saaliit;
     }
 
+    /*
+    Arpoo petokalan aloituskoon.
+
+    AE: true
+    */
     private static double arvoElionKoko() {
         double koko = minAlkuKoko + (rd.nextDouble() * (maxAlkuKoko - minAlkuKoko));
         return Math.round(koko * 10.0) / 10.0;
     }
 
+    /*
+    Luo uuden petokalan ja palauttaa sen.
+
+    AE: true
+    */
     public static Petokala luoPetokala(ArrayList<Elio> saaliit, ArrayList<Kala> Kala_saaliit) {
         Petokala eräs = new Petokala(arvoElionKoko(), arvoElionSukupuoli(), saaliit, Kala_saaliit);
         return eräs;
     }
+
+    /*
+    Petokala yrittää löytää ja syödä saaliin.
+
+    AE: Kala_saaliit.size() > 0
+    */
     public void syo(){
         int Kks = Kala_saaliit.size();
         if (Kks > 0){
